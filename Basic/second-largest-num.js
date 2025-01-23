b39a1b9b85a1266c673b529ea09edeed5d71fcbf
Pratap Git <pratapkumarnayak09@gmail.com>
2025-01-23 23:31:08 +0530

    merge two array object

--- Basic/second-largest-num.js
+++ Basic/second-largest-num.js
@@ -1,5 +1,5 @@
-let arr = [2,5,6,2,8,4]
-let sortArr = []
+let arr = [2,5,6,2,8,4];
+let sortArr = [];
 function secondLargest(arr){
     sortArr = arr.sort()    
     return sortArr[sortArr.length-2]
